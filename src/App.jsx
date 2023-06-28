4355819076f0a4cef85b09a572b8eb16383f9abc
shanuka2000 <shanuka.inbox@gmail.com>
2023-06-29 01:10:40 +0530

    Added favourit product page and updated route stack

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import Product from "./pages/Product";
+import FavouritProducts from "./pages/FavouritProducts";
 
 function App() {
   return (
     <Container>
       <Router>
         <Routes>
+          <Route path="/favourite" element={<FavouritProducts />} />
           <Route path="/" element={<Product />} />
         </Routes>
       </Router>
---
+++ src/pages/FavouritProducts.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const FavouritProducts = () => {
+  return <Container>FavouritProducts</Container>;
+};
+
+export default FavouritProducts;
+
+const Container = styled.div``;
